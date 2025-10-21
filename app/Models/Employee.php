e7d600e3b23ee6eb71ff92b254cd24b05dec9c2e
gearyfh <vincent.gearyf@gmail.com>
2025-10-21 11:09:39 +0700

    fix

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -41,7 +41,7 @@ class Employee extends Model
         return $this->hasMany(Document::class, 'employee_id');
     }
 
-    public function overtimeRequests()
+    public function overtimeRequest()
 {
     return $this->hasMany(OvertimeRequest::class);
 }
